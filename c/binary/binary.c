a86e1457ab142098da78508f749562609dee23f2
chcknlegwill <willn70@outlook.com>
2024-09-14 20:08:33 +0100

    Solved binary on the c track

--- c/binary/binary.c
+++ c/binary/binary.c
@@ -2,6 +2,7 @@
 #include "stdio.h"
 #include <string.h>
 
+
 int convert(const char *input) {
 
 //need to work on filtering out non-binary characters 
@@ -10,36 +11,55 @@ int convert(const char *input) {
 //----<>char char[] = strlen(input);
 //size_t i = -1;
 
-//need to reverse string
+//(didnt need to reverse the string at all...)
+int result = 0;
 
-int steps = 0;
-char n = strlen(input);
-for (int i = 0; i < strlen(input); i++) {
-  steps++;
-  printf("step: %d", steps);
+if (input == NULL || *input == '\0') {
+  return -1;
+}
 
-  printf("i: %c", input[i]);
-  printf("\n");
+const char *i = input;
+while (*i == '0') {
+  i++;
+  //skip leading zeros as zeros don't add anything
+}
+if(*i == 0) {
+  return 0;
+  //end of program
+}
+
+
+while(*i != '\0') {
+  if(*i == '0' || *i == '1') {
+    result = result * 2 + (*i - '0');
+  } else {
+    return -1;
+  }
+  i++;
 }
 
 //128, 64, 32, 16, 8, 4,| 2, 1   (start at 1 until 128 (step 8 - n * 2))
 // ^(8)<---------------...(1)^--------|
-  return 0;
+  return result;
 }
+
+/*
+//didn't need a for loop at all xD
 int main() {
-  //printf("ruh moment");
-  //printf("%d", convert("101010"));
 
-//               1-:4, 0-:2, 1-: 1
-  convert("101");
-  //should return 5!!!
+  char one[] = "101";
+  printf("Binary: %s, Decimal: %d\n", one, convert("1010"));
 
-printf("==========================\n");
-  convert("101010");
-  //should return 42!!
+  char two[] = "101010";
+  printf("Binary %s, Decimal %d\n", two, convert(two));
+
+  char invalid[] = "bruh";
+  printf("Should be invalid, %s %d \n", two, convert(invalid));
 
   //printf("\n");
   //printf("test %c", convert("A10101"));
 
   return 0;
 }
+
+*/
