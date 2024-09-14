90364a5e2419f917a632ccb3816bd8acd6452839
chcknlegwill <willn70@outlook.com>
2024-09-14 19:06:59 +0100

    Changing stuff for now

--- c/binary/binary.c
+++ c/binary/binary.c
@@ -7,58 +7,39 @@ int convert(const char *input) {
 //need to work on filtering out non-binary characters 
 // ^-> loop through the whole input array (cuz that's whats stored
 //in the array).
-int p = strlen(input);
+//----<>char char[] = strlen(input);
 //size_t i = -1;
 
-for (int c = p; c != '\0'; c = ++p) {
-  /*
-  if(input[c] != *"0" || input[c] != *"1") {
-    printf("err");
-    return -1;
-  } */
-  printf("%c", input[c]);
-  //printf("%c\n", c);
-} 
-//                      
+//need to reverse string
+
+int steps = 0;
+char n = strlen(input);
+for (int i = 0; i < strlen(input); i++) {
+  steps++;
+  printf("step: %d", steps);
+
+  printf("i: %c", input[i]);
+  printf("\n");
+}
+
 //128, 64, 32, 16, 8, 4,| 2, 1   (start at 1 until 128 (step 8 - n * 2))
 // ^(8)<---------------...(1)^--------|
-
-
-
-
-
-
-/*
-for (i = 0; i < length; i++) {
-  printf("%c\n", input[i]);
-  if (input[i] == *" " || input[i] != *"0" || input[i] != *"1") {
-    printf("BEUHMOEMENT");
-    return -1;
-  } //checks input ig
-}
-*/
-/*
-while (input[i] != '\0') {
-  printf("%c", input[i]);
-  i++;
-}*/
-
-
-  //printf("input: %s", input);
-  // printf("convert bruh");
-
-
-  //compilingo \||/
   return 0;
 }
-
-
 int main() {
   //printf("ruh moment");
   //printf("%d", convert("101010"));
 
+//               1-:4, 0-:2, 1-: 1
+  convert("101");
+  //should return 5!!!
+
+printf("==========================\n");
+  convert("101010");
+  //should return 42!!
+
   //printf("\n");
-  printf("test %c", convert("A10101"));
+  //printf("test %c", convert("A10101"));
 
   return 0;
 }
