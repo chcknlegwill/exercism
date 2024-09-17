86662ac5e7493009c672cbcdb10ff751695de104
William <willn70@outlook.com>
2024-09-18 00:09:37 +0100

    Gor darts compling on the C track

---
+++ c/darts/darts.c
@@ -0,0 +1,17 @@
+#include "darts.h"
+#include <stdio.h>
+#include <stdint.h>
+#include <math.h>
+
+uint8_t score(coordinate_t landing_position) {
+    float distance = 0;
+
+    return 0;
+}
+
+int main() {
+    printf("bru moment");
+    //compiling!!!
+    //had to modify the .h file - still don'r know what im doing too much but definitely enjoying it!
+    return 0;
+}
