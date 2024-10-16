fb1d899736907ce13884b61484396a5a77ba1f5c
chcknlegwill <willn70@outlook.com>
2024-10-16 01:25:10 +0100

    Successfully completed the darts exercise - those C coordinates were fun to work with.

--- c/darts/darts.c
+++ c/darts/darts.c
@@ -1,10 +1,43 @@
 #include "darts.h"
 #include <math.h>
 #include <stdio.h>
-
+#include <stdint.h>
 
 uint8_t score(coordinate_t landing_position) {
 
-  landing_position == { 1.0F, 2.0F };
-  return 0;
+  float distance = sqrt(landing_position.x * landing_position.x + landing_position.y * landing_position.y);
+  int currentScore = 0;
+  //works!
+
+
+
+
+
+/*
+ //score
+ if(distance > 10 ) {
+  currentScore = 0;
+ } else if(distance <= 10 ) {
+  currentScore = 1;  
+ } else if(distance <= 5) {
+  currentScore = 5;
+ } else :test_exactly_on_center:FAIL: Expected 10 Was 1
+distance: 0.141421currentScore: 1./test_darts.c:57ld also just return the score straight rather than storing it in a variable;
+
+*/
+  if(distance <= 1) {    //exactly on center first this tim
+    return 10;
+  } else if(distance <= 5) { //inner circle
+    return 5;
+  } else if(distance <= 10) {
+    return 1;
+  } else if(distance > 10) {
+    return 0;
+  }
+
+
+ printf("distance: %f\n", distance); 
+ printf("currentScore: %d\n", currentScore );
+
+  return currentScore;
 }
