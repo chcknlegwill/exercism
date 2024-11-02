89afacc44535e53d8c0c54e28a40c685cb6f2847
William <WillN70@outlook.com>
2024-11-02 06:01:17 +0000

    Further improved the binary_search exercise by using a half-open interval.

--- c/binary-search/binary_search.c
+++ c/binary-search/binary_search.c
@@ -4,29 +4,26 @@
 #include <stdbool.h>
 #include <string.h>
 //Thanks to siebenschlaefer for advice / mentoring to improve this solution. 
-
-//Did not need the bubble sort
+//^Made additional changes using a 
 
 const int* binary_search(int value, const int* arr, size_t length) {
-    if (arr == NULL || length == 0) {
-      return NULL;
-    }
     //this function should run in sub-linear time O(log n)
 
-    //binary search
-    int left = 0;
-    int right = length - 1;
+    //binary search change left & right to size_t
+    size_t left = 0;
+    size_t right = length; //assign length to right
 
-    while (left <= right) {
-      int mid = left + (right - left) / 2;
+    while (left < right) {
+      size_t mid = left + (right - left) / 2;
       if (arr[mid] == value) {
         return &arr[mid];
-          }
+        }
       if(arr[mid] < value) {
         left = mid + 1;
-      } else {
-        right = mid - 1;
-      }
+        } 
+      else {
+        right = mid;
+        } //^don't - 1 anymore as the type has changed.
     }
     return NULL;
 }
@@ -77,5 +74,4 @@ int main() { //uncomment the func before for debugging
 
 
 }
-
 */
