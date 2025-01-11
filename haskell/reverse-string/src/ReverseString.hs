5c0a7b861b3d1b386044fb37a51211357b84615d
chcknlegwill <willn70@outlook.com>
2025-01-11 07:09:30 +0000

    Successfully completed reverse string on the Haskell path.

---
+++ haskell/reverse-string/src/ReverseString.hs
@@ -0,0 +1,8 @@
+module ReverseString (reverseString) where
+
+reverseString :: String -> String
+-- code here:
+--string essentialy a list of characters (chars)
+reverseString str = reverse str
+--making use of the reverse function above
+reverseString str = error "You need to implement this function."
