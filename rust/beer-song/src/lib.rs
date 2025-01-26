418182be5bc590f397eb3ff177dfa5b4fd814e8f
William <WillN70@outlook.com>
2025-01-26 16:50:53 +0000

    From laptop.

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,5 +1,7 @@
 pub fn verse(n: u32) -> String {
-    todo!("emit verse {n}")
+    //todo!("emit verse {n}");
+    //I'm guessing n is the number of verses to sing 
+    sing(n);
 }
 
 pub fn sing(start: u32, end: u32) -> String {
--- rust/collatz-conjecture/src/lib.rs
+++ rust/collatz-conjecture/src/lib.rs
@@ -2,8 +2,6 @@
 pub fn collatz(n: u64) -> Option<u64> {
     //todo!("return Some(x) where x is the number of steps required to reach 1 starting with {n}")
 
-
-
     if n == 0 {
       return None;
     }
