9fd055c816375c9c985c5f2eef9b5aee1239cf15
William <WillN70@outlook.com>
2025-01-27 17:13:47 +0000

    Cleaned up code for beer-song from sugguestions by the exercism Rust analysis.

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -3,26 +3,15 @@
 pub fn verse(n: u32) -> String {
   //println!("{}", n);
   if n == 0 {
-    return String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n");
+    String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")
   } else if n == 1 {
-    return String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n")
+    String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n")
   } else {
     let b = n - 1;
     let bottles = if b == 1 { "bottle" } else { "bottles" }; 
-    return format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {b} {bottles} of beer on the wall.\n");
+    format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {b} {bottles} of beer on the wall.\n")
   }
 
-  /* 
-  let b = x - 1;
-  while x > 0 {
-    let bottles  = if b == 1 { "bottle "} else { "bottles"}; 
-    //println!("{}", n);
-    //let b = x - 1;
-    //format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {b} bottles of beer on the wall.\n");
-    x = x - 1;
-  }
-  return format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {b} bottle of beer on the wall.\n");
-  */
 }
 
 pub fn sing(start: u32, end: u32) -> String {
@@ -35,14 +24,13 @@ pub fn sing(start: u32, end: u32) -> String {
     println!("{}", c);
     
     if c > end {
-      song.push_str("\n");
+      song.push('\n'); //changed due to exercism 
     }
     if c > end {
-      c = c - 1;
+      c -= 1;
     } else { break; }
   }
-  return song;
-  //todo!("sing verses {start} to {end}, inclusive");
+  song
 }
 
 
