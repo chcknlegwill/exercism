6ae664615997fe12a1a298ca5ba40b0cc5b994c9
chcknlegwill <willn70@outlook.com>
2025-01-28 01:05:27 +0000

    Successfully completed raindrops & started gigasecond on the Rust track.

---
+++ rust/gigasecond/src/lib.rs
@@ -0,0 +1,11 @@
+use time::PrimitiveDateTime as DateTime;
+//https://docs.rs/time/latest/time/
+
+// Returns a DateTime one billion seconds after start.
+pub fn after(start: DateTime) -> DateTime {
+    //todo!("What time is a gigasecond later than {start}");
+    //gigasecond is a thousand mllion seconds 
+    let mut date = DateTime::new { }; //need to look at the docs for this
+
+    return date; 
+}
--- rust/raindrops/src/lib.rs
+++ rust/raindrops/src/lib.rs
@@ -1,13 +1,29 @@
 pub fn raindrops(n: u32) -> String {
     //todo!("what sound does Raindrop #{n} make?")
     //may need to declare a string and add results into it and then
-    //return the whole string
-    let mut _x = n;
-    String result = "";
-    if n / 3 == 0 { //n is divisible by 3
-      String::from("Pling")
-    } else {
-      String::from("{n}")
+    //return the whole string -> maybe use string.push() ??
+    //
+    //trying to use vectors for the strings
+    let mut chars = String::new();
+
+    if n == 1 {
+        chars.push_str(&n.to_string()); //chars.push_str working!!!!
+        return chars;
+    } 
+    if n % 3 == 0 { //n is divisible by 3.
+        chars.push_str("Pling");
     }
+    if n % 5 == 0 {
+        chars.push_str("Plang");
+    } 
+    if n % 7 == 0 {
+        chars.push_str("Plong");
+    } 
+    if chars.is_empty() {
+        chars.push_str(&n.to_string());
+    } else {
+        return chars;
+    }
+    chars
 
 }
