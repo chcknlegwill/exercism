8b1592b7d289c09cf1b0cee258ee43e7f74c1c61
chcknlegwill <willn70@outlook.com>
2025-02-08 17:13:38 +0000

    Successfully completed prime-numbers on the Rust trackgit add --all

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,3 +1,35 @@
 pub fn factors(n: u64) -> Vec<u64> {
-    todo!("This should calculate the prime factors of {n}")
+  //todo!("This should calculate the prime factors of {n}")
+  //tests are expecting a (list)? of numbers to be returned
+
+  let mut a = n; //mutable copy of n
+  let mut vec = Vec::new();
+  //vec.push(1);
+  //println!("{}", n);
+
+  if n == 0 || n == 1 {
+    return vec //no factors (base case)
+  }
+  
+  while a % 2 == 0 { //while even
+    vec.push(2);
+    a /= 2;
+  }
+
+  let mut b = 3;
+  while b * b <= a { //while odd
+    while a % b == 0 {
+      vec.push(b);
+      a /= b;
+    }
+    b += 2;
+  }
+
+  //after both odd and even while statements
+  //if a is a prime number, it should be above 1 
+  if a > 1 {
+    vec.push(a);
+  }
+
+  return vec;
 }
