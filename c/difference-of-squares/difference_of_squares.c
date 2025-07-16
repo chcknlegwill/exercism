18a50c63d0f33f747540f3d16b0e8e7a64c4e3b4
chcknlegwill <willn70@outlook.com>
2025-07-16 03:41:22 +0100

    Adding a new problem, difference of squares - resistor-color is a lost cause

---
+++ c/difference-of-squares/difference_of_squares.c
@@ -0,0 +1,25 @@
+#include "difference_of_squares.h"
+#include <stdio.h>
+
+
+
+unsigned int sum_of_squares(unsigned int number)  {
+  return number;
+}
+
+unsigned int square_of_sum(unsigned int number) {
+  int num = number;
+  //int[] nums_to_sum;
+  int sum = 0;
+  for (int i = 1; i <= num; i++) {
+    sum += i;   
+    printf("%d", i); 
+    printf("sum = %d\n", sum); 
+  } //we have 15!!!! we are getting somewhere
+  return number;
+}
+
+unsigned int difference_of_squares(unsigned int number) {
+  return number;
+}
+
--- c/resistor-color/resistor_color.c
+++ c/resistor-color/resistor_color.c
@@ -1,32 +1,14 @@
 #include "resistor_color.h"
 #include <stdio.h>
 
-// Define color constants
-#define BLACK 0
-#define BROWN 1
-#define RED 2
-#define ORANGE 3
-#define YELLOW 4
-#define GREEN 5
-#define BLUE 6
-#define VIOLET 7
-#define GREY 8
-#define WHITE 9
-
-
 resistor_band_t color_code(resistor_band_t band) {
-  // declare colors:
-  const char* colors[]  = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
-    printf("color at index is %s \n", colors[band]);
     return band;
-  }
+}
 
-
-/*
-  const resistor_band_t colors(void) {
-    const resistor_band_t colors[]  = {"black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"};
-    return colors;
-
-  }
-*/
-
+const resistor_band_t* colors(void) {
+    static const resistor_band_t color_array[] = {
+        BLACK, BROWN, RED, ORANGE, YELLOW,
+        GREEN, BLUE, VIOLET, GREY, WHITE
+    };
+    return color_array;
+}
