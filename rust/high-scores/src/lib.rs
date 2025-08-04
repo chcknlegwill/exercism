041589fa76eb6c14bf76163831a148d3f253002b
chcknlegwill <willn70@outlook.com>
2025-08-04 19:20:01 +0100

    Added new exercise high-scores on the Rust track.

---
+++ rust/high-scores/src/lib.rs
@@ -0,0 +1,32 @@
+#[derive(Debug)]
+pub struct HighScores {
+  scores: Vec<u32>,
+}
+
+impl HighScores {
+  pub fn new(scores: &[u32]) -> Self {
+    //todo!("Construct a HighScores struct, given the scores: {scores:?}")
+    println!("Scores: {:?}", scores);
+    HighScores {
+      scores: scores.to_vec(),
+    }
+  }
+
+
+  pub fn scores(&self) -> &[u32] {
+    todo!("Return all the scores as a slice")
+  }
+
+  pub fn latest(&self) -> Option<u32> {
+    todo!("Return the latest (last) score")
+  }
+
+  pub fn personal_best(&self) -> Option<u32> {
+    todo!("Return the highest score")
+  }
+
+  pub fn personal_top_three(&self) -> Vec<u32> {
+    todo!("Return 3 highest scores")
+  }
+
+}
