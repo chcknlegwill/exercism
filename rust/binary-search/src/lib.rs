cb47d6c13d60677ee97b18c8c5b6c947ff236d80
chcknlegwill <willn70@outlook.com>
2025-09-15 01:39:13 +0100

    Started binary-search on the rust track - will finish later

---
+++ rust/binary-search/src/lib.rs
@@ -0,0 +1,9 @@
+pub fn find(array: &[i32], key: i32) -> Option<usize> {
+    //todo!("Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index.");
+    if array.is_empty() {
+      return None;
+    }
+    let bruh = 1;
+    println!("'{key}'");
+    return Some(bruh);
+}
