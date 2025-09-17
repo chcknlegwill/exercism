c13d7ab6fb2cce66f0256f995b6f67d49d20f45d
chcknlegwill <willn70@outlook.com>
2025-09-18 00:08:41 +0100

    Finished binary-search on the rust track

--- rust/binary-search/src/lib.rs
+++ rust/binary-search/src/lib.rs
@@ -1,9 +1,30 @@
+//using the same while loop as I did for the C track.
+//Time-complexity: O(log n)
+//Space-complexity: O(1)
+
 pub fn find(array: &[i32], key: i32) -> Option<usize> {
-    //todo!("Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index.");
-    if array.is_empty() {
+  //todo!("Using the binary search algorithm, find the element '{key}' in the array '{array:?}' and return its index.");
+  if array.is_empty() {
       return None;
     }
-    let bruh = 1;
-    println!("'{key}'");
-    return Some(bruh);
+    //println!("'{key}'");
+    let length = array.len();
+
+    let mut left = 0;
+    let mut right = length - 1;
+    while left <= right {
+      let mid = left + (right - left) / 2;
+      if array[mid] == key {
+        return Some(mid);
+      }
+      if array[mid] < key {
+        left = mid + 1;
+      } else {
+        if mid == 0 {
+          return None;
+        }
+        right = mid -1;
+      }
+    }
+    return None;
 }
